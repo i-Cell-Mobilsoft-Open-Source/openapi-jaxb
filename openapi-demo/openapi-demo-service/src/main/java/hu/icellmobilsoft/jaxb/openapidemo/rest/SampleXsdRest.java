4d9d26c63ba25b0f86f9fdef9d6675ae90635592
Márk Petrényi <mark.petrenyi@icellmobilsoft.hu>
2019-02-05 16:23:19 +0100

    Translate documentation

--- openapi-demo/openapi-demo-service/src/main/java/hu/icellmobilsoft/jaxb/openapidemo/rest/SampleXsdRest.java
+++ openapi-demo/openapi-demo-service/src/main/java/hu/icellmobilsoft/jaxb/openapidemo/rest/SampleXsdRest.java
@@ -10,11 +10,11 @@ import hu.icellmobilsoft.openapi.dto.sample.sample.SampleType;
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-@Tag(name = "OpenAPI xsd példa", description = "Példa rest service xsd-ből generált objektumokkal")
+@Tag(name = "OpenAPI xsd example", description = "Rest service example with objects generated from xsd")
 @Path("/sample")
 public class SampleXsdRest {
 
-    @Operation(summary = "OpenAPI xsd példa", description = "Példa rest service xsd-ből generált objektumokkal")
+    @Operation(summary = "OpenAPI xsd operation example", description = "Rest operation example with objects generated from xsd")
     @POST
     @Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_XML, MediaType.APPLICATION_XML})
     @Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_XML, MediaType.APPLICATION_XML})
