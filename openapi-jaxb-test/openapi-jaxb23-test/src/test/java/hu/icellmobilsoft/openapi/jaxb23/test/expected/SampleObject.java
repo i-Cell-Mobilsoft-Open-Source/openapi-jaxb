a91f2cf53ef4ba55ced51ec7cce7bee1a8bb8fd2
Márk Petrényi <mark.petrenyi@icellmobilsoft.hu>
2022-12-19 14:28:04 +0100

    LM-1169 move demo dto to test modul

---
+++ openapi-jaxb-test/openapi-jaxb23-test/src/test/java/hu/icellmobilsoft/openapi/jaxb23/test/expected/SampleObject.java
@@ -0,0 +1,57 @@
+/*-
+ * #%L
+ * OpenAPI JAXB Plugin
+ * %%
+ * Copyright (C) 2019 - 2022 i-Cell Mobilsoft Zrt.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package hu.icellmobilsoft.openapi.jaxb23.test.expected;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Schema(name = "SampleObject", description = "Sample object (typeDoc)")
+public class SampleObject {
+
+    @Schema(name = "property", title = "property", description = "\n\nRestrictions: \n* maxLength: 50\n* minLength: 10\n* pattern: .*[^\\s].*",
+            required = true, maxLength = 50, minLength = 10, pattern = ".*[^\\s].*")
+    protected String property;
+
+    /**
+     * Gets the value of the property property.
+     * 
+     * @return possible object is {@link String }
+     * 
+     */
+    public String getProperty() {
+        return property;
+    }
+
+    /**
+     * Sets the value of the property property.
+     * 
+     * @param value
+     *            allowed object is {@link String }
+     * 
+     */
+    public void setProperty(String value) {
+        this.property = value;
+    }
+
+    @Schema(hidden = true)
+    public boolean isSetProperty() {
+        return (this.property != null);
+    }
+
+}
